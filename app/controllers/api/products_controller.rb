9c832015319fa0687956246ba16a718b0f13a5b7
Arthur PV <apetersonveatch@gmail.com>
2020-10-28 13:57:08 -0500

    adds individual pages for all three products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all.json.jb"
   end
+
+  def spoon
+    @spoon = Product.first
+    render "spoon.json.jb"
+  end
+
+  def tape
+    @tape = Product.second
+    render "tape.json.jb"
+  end
+
+  def chair
+    @chair = Product.third
+    render "chair.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#displayall"
+    get "/spoon" => "products#spoon"
+    get "/tape" => "products#tape"
+    get "/chair" => "products#chair"
   end
 end
